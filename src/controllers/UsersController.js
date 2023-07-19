fc9156b130a69cb4a086e64dd6a836412af28dfc
viniciuspra <vinicius.cascaesp@gmail.com>
2023-07-19 19:24:18 -0300

    feat: Add password encryption with bcryptjs

--- src/controllers/UsersController.js
+++ src/controllers/UsersController.js
@@ -1,16 +1,26 @@
-const AppError = require("../utils/AppError")
+const { hash } = require("bcryptjs");
+const knex = require("../database/knex");
+const AppError = require("../utils/AppError");
 
 class UsersController {
   async create(request, response) {
-    const { name, email, password } = request.body
+    const { name, email, password } = request.body;
 
-    if (!name) {
-      throw new AppError("Nome é obrigatório!")
+    const userWithEmail = await knex("users").where({ email }).first();
+    if (userWithEmail) {
+      throw new AppError("Este E-mail ja está em uso!");
     }
-    
-    return response.json({ name, email, password })
-  }
 
+    const hashedPassword = await hash(password, 8);
+
+    await knex("users").insert({
+      name,
+      email,
+      password: hashedPassword,
+    });
+
+    return response.json();
+  }
 }
 
-module.exports = UsersController
+module.exports = UsersController;
