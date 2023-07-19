301e428c3570c17fdc6c0323aca83d125135e09d
viniciuspra <vinicius.cascaesp@gmail.com>
2023-07-19 19:24:55 -0300

    feat: Add dishes route to main index route

--- src/routes/index.js
+++ src/routes/index.js
@@ -1,7 +1,9 @@
 const { Router } = require('express')
-const usersRouter = require('./user.routes');
+const usersRouter = require('./users.routes')
+const dishesRouter = require('./dishes.routes')
 
 const routes = Router()
-routes.use("/users", usersRouter)
+routes.use('/users', usersRouter)
+routes.use('/dishes', dishesRouter)
 
 module.exports = routes
