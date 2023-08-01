ab71827e7e232dbb64ccd7b2a16d2fbe1bfc0591
viniciuspra <vinicius.cascaesp@gmail.com>
2023-07-31 21:34:31 -0300

    feat: Create SessionsController

---
+++ src/controllers/SessionsController.js
@@ -0,0 +1,11 @@
+const knex = require("../database/knex");
+const AppError = require("../utils/AppError");
+const { compare } = require("bcryptjs");
+
+class SessionsController {
+  async create(request, response) {
+    
+  }
+}
+
+module.exports = SessionsController;
