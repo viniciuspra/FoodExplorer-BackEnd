f228b2d5511e9ffc5899c92f75f5f494c9e50eef
viniciuspra <vinicius.cascaesp@gmail.com>
2023-08-02 16:20:11 -0300

    feat: Add middleware to Dishes routes

--- src/routes/dishes.routes.js
+++ src/routes/dishes.routes.js
@@ -1,13 +1,16 @@
 const { Router } = require('express')
 
 const DishesController = require('../controllers/DishesController');
+const ensureAuthenticated = require('../middlewares/ensureAuthenticated');
 
 const dishesController = new DishesController()
 
 const dishesRoutes = Router()
 
+dishesRoutes.use(ensureAuthenticated)
+
+dishesRoutes.post('/', dishesController.create)
 dishesRoutes.get('/', dishesController.index)
-dishesRoutes.post('/:user_id', dishesController.create)
 dishesRoutes.put('/:id', dishesController.update)
 dishesRoutes.get('/:id', dishesController.show)
 dishesRoutes.delete('/:id', dishesController.delete)
