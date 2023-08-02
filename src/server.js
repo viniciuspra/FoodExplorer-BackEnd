20ea23935193ed6f61072c716ebe501a88e06555
viniciuspra <vinicius.cascaesp@gmail.com>
2023-08-02 17:24:49 -0300

    feat: Install cors

--- src/server.js
+++ src/server.js
@@ -4,7 +4,10 @@ const AppError = require('./utils/AppError')
 const express = require('express')
 const routes = require('./routes')
 
+const cors = require('cors')
+
 const app = express()
+app.use(cors())
 app.use(express.json())
 app.use(routes)
 app.use(( error, request, response, next ) => {
