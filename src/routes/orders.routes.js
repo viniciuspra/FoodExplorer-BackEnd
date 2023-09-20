2054e597b7129b957a6fd6bea93899c44ea95e13
Vinicius Cascaes Pra <vinicius.cascaesp@gmail.com>
2023-09-20 14:35:53 -0300

    feat: Add routes to orderRoutes

--- src/routes/orders.routes.js
+++ src/routes/orders.routes.js
@@ -3,6 +3,7 @@ const { Router } = require('express')
 const OrdersController = require("../controllers/OrdersController");
 
 const ensureAuthenticated = require("../middlewares/ensureAuthenticated");
+const ensureIsAdmin = require("../middlewares/ensureIsAdmin");
 
 const ordersController = new OrdersController();
 
@@ -11,6 +12,8 @@ const orderRoutes = Router();
 orderRoutes.use(ensureAuthenticated);
 
 orderRoutes.post("/", ordersController.create);
-orderRoutes.put("/:id", ordersController.update);
+orderRoutes.put("/:id", ensureIsAdmin, ordersController.update);
+orderRoutes.get("/", ordersController.index);
+orderRoutes.delete("/:id", ordersController.delete);
 
 module.exports = orderRoutes
